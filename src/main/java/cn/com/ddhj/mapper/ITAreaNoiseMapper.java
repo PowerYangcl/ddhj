223cb73533b47112b12e61129e3691cae075a1e8
yangchenglin <yangchenglin@huijiayou.cn>
2016-10-14 16:43:47 +0800

    2

---
+++ src/main/java/cn/com/ddhj/mapper/ITAreaNoiseMapper.java
@@ -0,0 +1,6 @@
+package cn.com.ddhj.mapper;
+
+
+public interface ITAreaNoiseMapper {
+
+}
---
+++ src/main/java/cn/com/ddhj/model/TAreaNoise.java
@@ -0,0 +1,73 @@
+package cn.com.ddhj.model;
+
+public class TAreaNoise {
+    private Integer zid;
+
+    private String area;
+
+    private Integer flag;
+
+    private String name;
+
+    private Double lat;
+
+    private Double lng;
+
+    private String address;
+
+    public Integer getZid() {
+        return zid;
+    }
+
+    public void setZid(Integer zid) {
+        this.zid = zid;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area == null ? null : area.trim();
+    }
+
+    public Integer getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Integer flag) {
+        this.flag = flag;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+}
