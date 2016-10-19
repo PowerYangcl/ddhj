93be871208f7e7bcff968d9b00aa28c93296ee72
zht <zht@test.com>
2016-10-19 18:26:21 +0800

    ss

--- src/main/java/cn/com/ddhj/service/impl/orderpay/config/XmasPayConfig.java
+++ src/main/java/cn/com/ddhj/service/impl/orderpay/config/XmasPayConfig.java
@@ -48,8 +48,9 @@ public class XmasPayConfig{
 	public static String getPayGateReturnUrl() {
 ////		return config.bConfig("xmaspay.paygate_return_url");
 //		return "http://qhbeta-cfamily.qhw.yshqi.com/cfamily/manage/paygate_notify.ftl";
+//		return "http://api.sys.ecomapit.com/payNotify.htm";
 //		return "http://stockwyz.xicp.net/ddhj/payNotify.htm";
-		return "http://api.sys.ecomapit.com/payNotify.htm";
+		return "http://112.126.93.46:8080/ddhj/payNotify.htm";
 	}
 //	
 //	/**
