7963bc725396ddbc644741b70b09dd611978941e
zhanghaiyu <zhanghy@huijiayou.cn>
2016-10-26 17:25:04 +0800

    zhy

--- src/main/java/cn/com/ddhj/service/impl/report/TReportServiceImpl.java
+++ src/main/java/cn/com/ddhj/service/impl/report/TReportServiceImpl.java
@@ -352,24 +352,24 @@ public class TReportServiceImpl extends BaseServiceImpl<TReport, TReportMapper, 
 			Integer airLevel = 1;
 			// 水质量等级
 			Integer waterLevel = 1;
-			// if (StringUtils.isNotBlank(lp.getCity())) {
-			// JSONArray cityAirLevel = null;
-			// if (cityAir != null && cityAir.size() > 0) {
-			// cityAirLevel = cityAir;
-			// } else {
-			// cityAirLevel = this.getCityAirLevel();
-			// }
-			// if (cityAirLevel != null && cityAirLevel.size() > 0) {
-			// for (int i = 0; i < cityAirLevel.size(); i++) {
-			// JSONObject level = cityAirLevel.getJSONObject(i);
-			//
-			// if (StringUtils.equals(lp.getCity(), level.getString("city"))) {
-			// airLevel = level.getJSONObject("level").getInteger("air");
-			// waterLevel = level.getJSONObject("level").getInteger("water");
-			// }
-			// }
-			// }
-			// }
+			if (StringUtils.isNotBlank(lp.getCity())) {
+				JSONArray cityAirLevel = null;
+				if (cityAir != null && cityAir.size() > 0) {
+					cityAirLevel = cityAir;
+				} else {
+					cityAirLevel = this.getCityAirLevel();
+				}
+				if (cityAirLevel != null && cityAirLevel.size() > 0) {
+					for (int i = 0; i < cityAirLevel.size(); i++) {
+						JSONObject level = cityAirLevel.getJSONObject(i);
+
+						if (StringUtils.equals(lp.getCity(), level.getString("city"))) {
+							airLevel = level.getJSONObject("level").getInteger("air");
+							waterLevel = level.getJSONObject("level").getInteger("water");
+						}
+					}
+				}
+			}
 			// 垃圾设施等级
 			Integer rubbishLevel = 1;
 			if (StringUtils.isNotBlank(lp.getCity()) && StringUtils.isNotBlank(lp.getLat())
