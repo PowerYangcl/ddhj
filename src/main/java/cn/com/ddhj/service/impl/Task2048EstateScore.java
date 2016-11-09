e78d2e50b6cdb3009ca4ffa8c0a02993d8cd1550
yangchenglin <yangchenglin@huijiayou.cn>
2016-11-09 23:34:01 +0800

    2

--- src/main/java/cn/com/ddhj/service/impl/Task2048EstateScore.java
+++ src/main/java/cn/com/ddhj/service/impl/Task2048EstateScore.java
@@ -193,7 +193,7 @@ public class Task2048EstateScore implements Callable<TLandedProperty> {
 			return map.get(map.lastKey());
 		}
 		
-		if(StringUtils.isBlank(level) && !StringUtils.isAnyBlank(String.valueOf(slat) , String.valueOf(nlat) , String.valueOf(wlng) ,String.valueOf(elng))){
+		if(StringUtils.isBlank(level)  && slat != null && nlat != null &&  wlng != null &&  elng != null){
 			if((slat<lat && lat < nlat) &&(wlng < lng && lng < elng)){ // 五环里
 				level = "II类@50/60";
 			}else{ 	// 北京：五环外 |上海：外环外 |广州：外环外|天津：外环外|深圳：关外全部划为I类标准
@@ -236,7 +236,7 @@ public class Task2048EstateScore implements Callable<TLandedProperty> {
 				 for(TWaterEnviroment e : list){
 					 Integer d = CommonUtil.getMeterDistance(lat, lng, Double.valueOf(e.getLat()), Double.valueOf(e.getLng())); 
 					 map_.put(d, e);
-				 }
+				 } 
 				 TWaterEnviroment w = map_.get(map_.firstKey());
 				 String level = w.getOxygenquality();
 				 water.put("level", level);
