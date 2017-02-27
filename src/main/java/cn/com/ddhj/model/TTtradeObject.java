c37ad98b9a74c5100a3121aeae334c401edc6581
yangchenglin <yangchenglin@huijiayou.cn>
2017-02-27 18:11:16 +0800

    t_trade_object java相关类及mapper.xml

---
+++ src/main/java/cn/com/ddhj/model/TTtradeObject.java
@@ -0,0 +1,96 @@
+package cn.com.ddhj.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class TTtradeObject {
+    private Integer id;
+
+    private String uuid;
+
+    private String objectCode;
+
+    private String objectName;
+
+    private BigDecimal leverageRate;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Integer maxAmount;
+
+    private Integer minAmount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid == null ? null : uuid.trim();
+    }
+
+    public String getObjectCode() {
+        return objectCode;
+    }
+
+    public void setObjectCode(String objectCode) {
+        this.objectCode = objectCode == null ? null : objectCode.trim();
+    }
+
+    public String getObjectName() {
+        return objectName;
+    }
+
+    public void setObjectName(String objectName) {
+        this.objectName = objectName == null ? null : objectName.trim();
+    }
+
+    public BigDecimal getLeverageRate() {
+        return leverageRate;
+    }
+
+    public void setLeverageRate(BigDecimal leverageRate) {
+        this.leverageRate = leverageRate;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getMaxAmount() {
+        return maxAmount;
+    }
+
+    public void setMaxAmount(Integer maxAmount) {
+        this.maxAmount = maxAmount;
+    }
+
+    public Integer getMinAmount() {
+        return minAmount;
+    }
+
+    public void setMinAmount(Integer minAmount) {
+        this.minAmount = minAmount;
+    }
+}
