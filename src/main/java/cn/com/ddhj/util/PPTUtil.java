694aa0369399dba8ea7f6d6693506ab04aeaa096
zhanghaiyu <zhanghy@huijiayou.cn>
2017-03-01 11:43:35 +0800

    zhy

--- src/main/java/cn/com/ddhj/util/PPTUtil.java
+++ src/main/java/cn/com/ddhj/util/PPTUtil.java
@@ -18,7 +18,6 @@ import com.artofsolving.jodconverter.openoffice.connection.OpenOfficeConnection;
 import com.artofsolving.jodconverter.openoffice.connection.SocketOpenOfficeConnection;
 import com.artofsolving.jodconverter.openoffice.converter.OpenOfficeDocumentConverter;
 
-
 /**
  * 
  * 类: PPTUtil <br>
@@ -30,13 +29,17 @@ public class PPTUtil {
 
 	private static PPTUtil self;
 
-	private final static String TEMPLATE_FILE = "/opt/ddhj/report/ppt/template/report.ppt";
-	private final static String OUT_REPORT_PPT_PATH = "/opt/ddhj/report/ppt/";
-	private final static String OUT_REPORT_PDF_PATH = "/opt/ddhj/report/pdf/";
+	// private final static String TEMPLATE_FILE =
+	// "/opt/ddhj/report/ppt/template/report.ppt";
+	// private final static String OUT_REPORT_PPT_PATH =
+	// "/opt/ddhj/report/ppt/";
+	// private final static String OUT_REPORT_PDF_PATH =
+	// "/opt/ddhj/report/pdf/";
 
-//	private final static String TEMPLATE_FILE = "E:/report/ppt//template/report.ppt";
-//	private final static String OUT_REPORT_PPT_PATH = "E:/report/ppt/";
-//	private final static String OUT_REPORT_PDF_PATH = "E:/report/pdf/";
+	private final static String TEMPLATE_FILE = "E:/report/ppt//template/report.ppt";
+	private final static String OUT_REPORT_PPT_PATH = "E:/report/ppt/";
+	private final static String OUT_REPORT_PDF_PATH = "E:/report/pdf/";
+
 	public static PPTUtil instance() {
 		if (self == null) {
 			synchronized (PPTUtil.class) {
@@ -101,7 +104,7 @@ public class PPTUtil {
 		try {
 			File inputFile = new File(sourceFile);
 			if (!inputFile.exists()) {
-				return flag;
+				return false;// 找不到源文件, 则返回-1
 			}
 			// 如果目标路径不存在, 则新建该路径
 			File outputFile = new File(path);
@@ -109,23 +112,23 @@ public class PPTUtil {
 				outputFile.getParentFile().mkdirs();
 			}
 			// 这里是OpenOffice的安装目录
-//			String OpenOffice_HOME = "C:\\Program Files (x86)\\OpenOffice 4";
-			String OpenOffice_HOME = "/opt/openoffice4/";
+			String OpenOffice_HOME = "C:\\Program Files (x86)\\OpenOffice 4";
 			// 如果从文件中读取的URL地址最后一个字符不是 '\'，则添加'\'
 			if (OpenOffice_HOME.charAt(OpenOffice_HOME.length() - 1) != '\\') {
 				OpenOffice_HOME += "\\";
 			}
 			// 启动OpenOffice的服务
-//			String command = OpenOffice_HOME+ "program\\soffice.exe -headless-accept=\"socket,host=127.0.0.1,port=8100;urp;\"";
-			String command = OpenOffice_HOME+"program\\soffice -headless-accept=\"socket,host=127.0.0.1,port=8100;urp;\"-nofirststartwizard";
-			Runtime.getRuntime().exec(command);
+			String command = OpenOffice_HOME
+					+ "program\\soffice.exe -headless -accept=\"socket,host=127.0.0.1,port=8100;urp;\"";
+			Process pro = Runtime.getRuntime().exec(command);
 			// connect to an OpenOffice.org instance running on port 8100
 			connection = new SocketOpenOfficeConnection(8100);
 			connection.connect();
 			// convert
 			DocumentConverter converter = new OpenOfficeDocumentConverter(connection);
 			converter.convert(inputFile, outputFile);
-			flag = true;
+			// close the connection
+			pro.destroy();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
