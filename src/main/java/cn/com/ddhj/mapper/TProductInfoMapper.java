92fc95678afacc103eb058be3858c338b3e24585
Yangcl <794867067@qq.com>
2017-07-23 23:06:13 +0800

    t_user_addresss t_product_info

---
+++ src/main/java/cn/com/ddhj/mapper/TProductInfoMapper.java
@@ -0,0 +1,8 @@
+package cn.com.ddhj.mapper;
+
+import cn.com.ddhj.dto.BaseDto;
+import cn.com.ddhj.model.TProductInfo;
+
+public interface TProductInfoMapper extends BaseMapper<TProductInfo, BaseDto>{
+  
+}
---
+++ src/main/java/cn/com/ddhj/mapper/TUserAddresssMapper.java
@@ -0,0 +1,8 @@
+package cn.com.ddhj.mapper;
+
+import cn.com.ddhj.dto.BaseDto;
+import cn.com.ddhj.model.TUserAddresss;
+
+public interface TUserAddresssMapper extends BaseMapper<TUserAddresss, BaseDto>{
+	
+}
