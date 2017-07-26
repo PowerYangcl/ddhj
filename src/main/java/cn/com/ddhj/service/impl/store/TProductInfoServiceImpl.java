cc37f9818a950d2cc423197d79fd3abd3c1e9273
yangchenglin <yangchenglin@huijiayou.cn>
2017-07-26 15:11:18 +0800

    2

--- src/main/java/cn/com/ddhj/service/impl/store/TProductInfoServiceImpl.java
+++ src/main/java/cn/com/ddhj/service/impl/store/TProductInfoServiceImpl.java
@@ -1,10 +1,41 @@
 package cn.com.ddhj.service.impl.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONObject;
+
 import cn.com.ddhj.dto.store.TProductInfoDto;
 import cn.com.ddhj.mapper.TProductInfoMapper;
 import cn.com.ddhj.model.TProductInfo;
+import cn.com.ddhj.result.TProductInfoResult;
 import cn.com.ddhj.service.impl.BaseServiceImpl;
+import cn.com.ddhj.service.store.ITProductInfoService;
 
-public class TProductInfoServiceImpl extends BaseServiceImpl<TProductInfo, TProductInfoMapper, TProductInfoDto> {
+@Service
+public class TProductInfoServiceImpl extends BaseServiceImpl<TProductInfo, TProductInfoMapper, TProductInfoDto> implements ITProductInfoService{
 
+	@Autowired
+	private TProductInfoMapper mapper;
+
+	@Override
+	public JSONObject getProductInfo(String productCode) {
+		JSONObject re = new JSONObject();
+		
+		TProductInfoResult e = mapper.getProductInfo(productCode);
+		if(e == null){
+			re.put("status", "error");
+			re.put("msg", "未找到对应的商品信息");
+			return re;
+		}
+		
+		re.put("status", "success");
+		re.put("msg", "查询成功");
+		re.put("entity", e);
+		return re;
+	}
+	
+	
+	
+	
 }
--- src/main/java/cn/com/ddhj/service/store/ITProductInfoService.java
+++ src/main/java/cn/com/ddhj/service/store/ITProductInfoService.java
@@ -1,9 +1,12 @@
 package cn.com.ddhj.service.store;
 
+import com.alibaba.fastjson.JSONObject;
+
 import cn.com.ddhj.dto.store.TProductInfoDto;
 import cn.com.ddhj.model.TProductInfo;
 import cn.com.ddhj.service.IBaseService;
 
 public interface ITProductInfoService extends IBaseService<TProductInfo, TProductInfoDto> {
 
+	public JSONObject getProductInfo(String productCode);
 }
