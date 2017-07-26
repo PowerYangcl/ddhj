7b70f41d71373c556e73ee7e4896e129be41d652
yangchenglin <yangchenglin@huijiayou.cn>
2017-07-26 15:21:28 +0800

    2

---
+++ src/main/java/cn/com/ddhj/result/TProductInfoResult.java
@@ -0,0 +1,14 @@
+package cn.com.ddhj.result;
+
+import cn.com.ddhj.model.TProductInfo;
+
+public class TProductInfoResult extends TProductInfo{
+	private String picUrls;
+
+	public String getPicUrls() {
+		return picUrls;
+	}
+	public void setPicUrls(String picUrls) {
+		this.picUrls = picUrls;
+	}
+}
