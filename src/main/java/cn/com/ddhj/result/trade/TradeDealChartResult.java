98d8ebbdb451c152ee347a7540ffab8f9f951f3b
hudson <hudson@test.com>
2017-08-04 15:05:38 +0800

    a

--- src/main/java/cn/com/ddhj/result/trade/TradeDealChartResult.java
+++ src/main/java/cn/com/ddhj/result/trade/TradeDealChartResult.java
@@ -1,20 +1,43 @@
 package cn.com.ddhj.result.trade;
 
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import cn.com.ddhj.base.BaseResult;
 import cn.com.ddhj.model.trade.TTradeDeal;
 
 public class TradeDealChartResult extends BaseResult {
-	Map<String, List<TTradeDeal>> deals = new ConcurrentHashMap<String, List<TTradeDeal>>();
-
-	public Map<String, List<TTradeDeal>> getDeals() {
-		return deals;
+	List<CityTradeDeals> list = new LinkedList<CityTradeDeals>();
+	
+	public List<CityTradeDeals> getList() {
+		return list;
+	}
+	public void setList(List<CityTradeDeals> list) {
+		this.list = list;
 	}
 
-	public void setDeals(Map<String, List<TTradeDeal>> deals) {
-		this.deals = deals;
+	public class CityTradeDeals {
+		private String cityName;
+		private List<TTradeDeal> deals = new LinkedList<TTradeDeal>();
+		public String getCityName() {
+			return cityName;
+		}
+		public void setCityName(String cityName) {
+			this.cityName = cityName;
+		}
+		public List<TTradeDeal> getDeals() {
+			return deals;
+		}
+		public void setDeals(List<TTradeDeal> deals) {
+			this.deals = deals;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			// TODO Auto-generated method stub
+			if(!(obj instanceof CityTradeDeals)) {
+				return false;
+			}
+			return cityName.equals(((CityTradeDeals)obj).getCityName());
+		}
 	}
 }
