1e5c135783a08030e6a5ac229af5a7812684c36e
Yangcl <794867067@qq.com>
2017-08-07 15:40:16 +0800

    2

--- src/main/java/cn/com/ddhj/service/impl/TAddressEnshrineServiceImpl.java
+++ src/main/java/cn/com/ddhj/service/impl/TAddressEnshrineServiceImpl.java
@@ -27,7 +27,16 @@ public class TAddressEnshrineServiceImpl implements ITAddressEnshrineService {
 	@Autowired
 	private ITUserService userService;
 	
-	@Override
+	/**
+	 * @descriptions 同步客户端收藏的地址到数据库
+	 * @测试地址如下：http://localhost:8080/ddhj/api.htm?apiTarget=rsync_address_enshrine&api_key=appfamilyhas&&apiInput={"lat":39.1439299,"lng":117.21081309,"level":"优","address":"天津市 河北区 民生路 52号","name":"龙亭家园","province":"天津市","city":"天津市","district":"河北区"}&api_timespan=2017-08-07+15:07:34&userToken=fc27799473fe4e828c8113a038c9d69a&api_secret=2f8f627488dbc05f822214d55561aa56
+	 *
+	 * @param input
+	 * @param userToken
+	 * @date 2017年8月7日 下午3:37:39
+	 * @author Yangcl 
+	 * @version 1.0.0.1
+	 */
 	public JSONObject rsyncAddressEnshrine(JSONObject input, String userToken) {
 		JSONObject re = new JSONObject();
 		
@@ -83,7 +92,16 @@ public class TAddressEnshrineServiceImpl implements ITAddressEnshrineService {
 	
 	
 	
-	@Override
+	/**
+	 * @descriptions 获取用户收藏地址列表
+	 * @测试地址如下：http://api.sys.ecomapit.com/ddhj/api.htm?apiTarget=address_enshrine_list&api_key=appfamilyhas&userToken=fc27799473fe4e828c8113a038c9d69a&api_secret=2f8f627488dbc05f822214d55561aa56
+	 *
+	 * @param userToken
+	 * @return
+	 * @date 2017年8月7日 下午3:39:19
+	 * @author Yangcl 
+	 * @version 1.0.0.1
+	 */
 	public JSONObject getUserAddressEnshrineList(String userToken) {
 		JSONObject re = new JSONObject();
 		String userCode = "";
@@ -102,7 +120,7 @@ public class TAddressEnshrineServiceImpl implements ITAddressEnshrineService {
 			re.put("resultMessage", "获取成功");
 			re.put("data", list);
 		}else{
-			re.put("resultCode", -1);
+			re.put("resultCode", 1);
 			re.put("resultMessage", "获取用户收藏地址列表为空");
 		}
 		
