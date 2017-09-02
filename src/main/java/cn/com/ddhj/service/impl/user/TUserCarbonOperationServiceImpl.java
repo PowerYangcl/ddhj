ececcf2b61a1f3057ce582b0d50530e2728db198
zhanghaitao <stockwyz@163.com>
2017-09-02 19:42:03 +0800

    工

--- src/main/java/cn/com/ddhj/service/impl/user/TUserCarbonOperationServiceImpl.java
+++ src/main/java/cn/com/ddhj/service/impl/user/TUserCarbonOperationServiceImpl.java
@@ -1,14 +1,17 @@
 package cn.com.ddhj.service.impl.user;
 
 import java.math.BigDecimal;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
@@ -258,71 +261,103 @@ public class TUserCarbonOperationServiceImpl
 	public DataResult findCarbonOperationDetail(TUserCarbonOperationDto dto, String userToken) {
 		DataResult result = new DataResult();
 		TUserLogin login = loginMapper.findLoginByUuid(userToken);
-		List<TUserCarbonOperation> list = null;
+//		List<TUserCarbonOperation> list = null;
 		if (login != null) {
 			TUser user = userMapper.findTUserByUuid(login.getUserToken());
 			if (user != null) {
 				dto.setUserCode(user.getUserCode());
 				if (dto.getPageIndex() != null && dto.getPageSize() != null) {
 					dto.setStart(dto.getPageIndex() * dto.getPageSize());
-					dto.setPageSize(dto.getPageSize());
+					//多拿出来2页的数据，如果有步行换碳币的记录要进行按日期合并
+					dto.setPageSize(dto.getPageSize() * 3);
 				}
 				
-				if(StringUtils.isBlank(dto.getOperationType()) 
-						&& StringUtils.isBlank(dto.getOperationTypeChild())) {
-					//前端按全部类型查询所有碳币收支记录
-					//先查询步行换碳币记录
-					list = new LinkedList<TUserCarbonOperation>();
-					dto.setOperationType(null);
-					dto.setOperationTypeChild("DC170208100004");
-					List<TUserCarbonOperation> oneList = mapper.findCarbonOperationDetailDailyOne(dto);
-					list.addAll(oneList);
-					//再查询其他类型换碳币记录
-					dto.setExcludeChildType("true");
-					dto.setOperationType(null);
-					dto.setOperationTypeChild("DC170208100004");
-					List<TUserCarbonOperation> otherList = mapper.findCarbonOperationDetail(dto);
-					list.addAll(otherList);
+				SimpleDateFormat sdf = new SimpleDateFormat("");
+				
+				Map<String, TUserCarbonOperation> stepMap = new HashMap<String, TUserCarbonOperation>();
+				List<TUserCarbonOperation> list = mapper.findCarbonOperationDetail(dto);
+				List<TUserCarbonOperation> delList = new LinkedList<TUserCarbonOperation>();
+				if(list != null) {
+					for(TUserCarbonOperation tco : list) {
+						if(tco.getOperationTypeChild().equals("DC170208100004")) {
+							//步行换碳币按日期合并
+							String date = tco.getCreateTime().substring(0, tco.getCreateTime().indexOf(" "));
+							if(stepMap.containsKey(date)) {
+								TUserCarbonOperation oneday = stepMap.get(date);
+								oneday.setCarbonSum(oneday.getCarbonSum().add(tco.getCarbonSum()).setScale(2, BigDecimal.ROUND_HALF_UP));
+								delList.add(tco);
+							} else {
+								stepMap.put(date, tco);
+							}
+						}
+					}
+					
+					list.removeAll(delList);
 					Collections.sort(list, new Comparator<TUserCarbonOperation>() {
 						@Override
 						public int compare(TUserCarbonOperation o1, TUserCarbonOperation o2) {
 							return o2.getCreateTime().compareTo(o1.getCreateTime());
 						}
 					});
-					
-					list = list.subList(0, dto.getPageSize() > list.size() ? list.size() : dto.getPageSize());
-					
-				} else if(StringUtils.isNotBlank(dto.getOperationTypeChild())) {
-					//按碳币小类型查
-					if(dto.getOperationTypeChild().equals("DC170208100004")) {
-						//按碳币小类型-步行换碳币查,按日期合并---因为同步的步行碳币记录过多
-						list = mapper.findCarbonOperationDetailDailyOne(dto);
-					} else {
-						//按碳币小类型查,不按日期合并
-						list = mapper.findCarbonOperationDetail(dto);
-					}
-				} else if(StringUtils.isNotBlank(dto.getOperationType())) {
-					//按碳币操作大类型查
-					if(dto.getOperationType().equals("DC170208100002")) {
-						//步行换碳币收入小类
-						dto.setOperationTypeChild("DC170208100004");
-						list = mapper.findCarbonOperationDetailDailyOne(dto);
-						//其他收入小类
-						dto.setExcludeChildType("true");
-						List<TUserCarbonOperation> otherList = mapper.findCarbonOperationDetail(dto);
-						list.addAll(otherList);
-						Collections.sort(list, new Comparator<TUserCarbonOperation>() {
-							@Override
-							public int compare(TUserCarbonOperation o1, TUserCarbonOperation o2) {
-								return o2.getCreateTime().compareTo(o1.getCreateTime());
-							}
-						});
-						
-						list = list.subList(0, dto.getPageSize() > list.size() ? list.size() : dto.getPageSize());
-					}
-					
-					list = mapper.findCarbonOperationDetail(dto);
 				}
+				
+				
+				
+//				if(StringUtils.isBlank(dto.getOperationType()) 
+//						&& StringUtils.isBlank(dto.getOperationTypeChild())) {
+//					//前端按全部类型查询所有碳币收支记录
+//					//先查询步行换碳币记录
+//					list = new LinkedList<TUserCarbonOperation>();
+//					dto.setOperationType(null);
+//					dto.setOperationTypeChild("DC170208100004");
+//					List<TUserCarbonOperation> oneList = mapper.findCarbonOperationDetailDailyOne(dto);
+//					list.addAll(oneList);
+//					//再查询其他类型换碳币记录
+//					dto.setExcludeChildType("true");
+//					dto.setOperationType(null);
+//					dto.setOperationTypeChild("DC170208100004");
+//					List<TUserCarbonOperation> otherList = mapper.findCarbonOperationDetail(dto);
+//					list.addAll(otherList);
+//					Collections.sort(list, new Comparator<TUserCarbonOperation>() {
+//						@Override
+//						public int compare(TUserCarbonOperation o1, TUserCarbonOperation o2) {
+//							return o2.getCreateTime().compareTo(o1.getCreateTime());
+//						}
+//					});
+//					
+//					list = list.subList(0, dto.getPageSize() > list.size() ? list.size() : dto.getPageSize());
+//					
+//				} else if(StringUtils.isNotBlank(dto.getOperationTypeChild())) {
+//					//按碳币小类型查
+//					if(dto.getOperationTypeChild().equals("DC170208100004")) {
+//						//按碳币小类型-步行换碳币查,按日期合并---因为同步的步行碳币记录过多
+//						list = mapper.findCarbonOperationDetailDailyOne(dto);
+//					} else {
+//						//按碳币小类型查,不按日期合并
+//						list = mapper.findCarbonOperationDetail(dto);
+//					}
+//				} else if(StringUtils.isNotBlank(dto.getOperationType())) {
+//					//按碳币操作大类型查
+//					if(dto.getOperationType().equals("DC170208100002")) {
+//						//步行换碳币收入小类
+//						dto.setOperationTypeChild("DC170208100004");
+//						list = mapper.findCarbonOperationDetailDailyOne(dto);
+//						//其他收入小类
+//						dto.setExcludeChildType("true");
+//						List<TUserCarbonOperation> otherList = mapper.findCarbonOperationDetail(dto);
+//						list.addAll(otherList);
+//						Collections.sort(list, new Comparator<TUserCarbonOperation>() {
+//							@Override
+//							public int compare(TUserCarbonOperation o1, TUserCarbonOperation o2) {
+//								return o2.getCreateTime().compareTo(o1.getCreateTime());
+//							}
+//						});
+//						
+//						list = list.subList(0, dto.getPageSize() > list.size() ? list.size() : dto.getPageSize());
+//					}
+//					
+//					list = mapper.findCarbonOperationDetail(dto);
+//				}
 				
 				if (list != null && list.size() > 0) {
 					result.setData(list);
